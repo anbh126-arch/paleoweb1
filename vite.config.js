0ef80c06c8fefc41db833ed4d7e4aa8fac527103
anbh126-arch <anbh126@gmail.com>
2026-05-17 16:16:32 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pro/',        //set base to name of your repository
+  base: '/paleoweb1/',        //set base to name of your repository
 })
 
